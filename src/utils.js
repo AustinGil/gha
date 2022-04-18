98bdc86af89001ec70bf8818c7ef27fee90f910b
Austin Gil <hey@austingil.com>
2022-04-18 15:20:49 -0700

    Oops!

--- src/utils.js
+++ src/utils.js
@@ -12,12 +12,12 @@ export const getType = (v) => {
  * @returns {boolean}
  */
 export let isType = (v, type) => {
-  return Object.prototype.toString.call(v).slice(8, -1).toLowerCase() === type;
+  return Object.prototype.toString.call(v).slice(8, -1).toLowerCase() === type
 };
 
 /**
  * @param {number} v
  */
 export const double = (v) => {
-  return v * 2;
+  return 2 * 2;
 };
